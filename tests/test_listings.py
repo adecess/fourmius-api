e0532cb06daec04b5a830c8faae7aad9ecb0b03e
Axel Saliniere <axel.saliniere@protonmail.com>
2023-11-22 10:47:47 +0100

    add listing fixture

--- tests/test_listings.py
+++ tests/test_listings.py
@@ -1,46 +1,54 @@
+import pytest
+
 from app.schemas.listing import ListingItem
 from .database import client, session
 
+listing_data = {
+    "latest_price": 400000,
+    "listing_url": "seloger.com/massivehouselille",
+    "location": "27 rue Solferino, Lille",
+    "rooms": 13,
+    "surface": 300,
+    "title": "Massive House Lille Vauban",
+    "type": "house",
+}
 
-def test_root(client):
-    response = client.get("/")
 
-    assert response.status_code == 200
-    assert response.json() == {
-        "message": "Welcome to Fourmius.io!",
-        "environment": "dev",
-        "testing": True,
-    }
+@pytest.fixture()
+def test_listing(client):
+    response = client.post("/listings/", json=listing_data)
+
+    assert response.status_code == 201
 
 
-def test_get_listings(client):
+def test_get_listings(client, test_listing):
     response = client.get("/listings/")
 
     # Then
     assert response.status_code == 200
-    assert response.json() == []
+    assert response.json() == [listing_data]
 
 
 def test_create_listing(client):
     response = client.post(
         "/listings/",
         json={
-            "latest_price": 400000,
-            "listing_url": "seloger.com/massivehouselille",
-            "location": "27 rue Solferino, Lille",
-            "rooms": 13,
-            "surface": 300,
-            "title": "Massive House Lille Vauban",
-            "type": "house",
+            "latest_price": 500000,
+            "listing_url": "seloger.com/massiveflatlille",
+            "location": "28 rue Solferino, Lille",
+            "rooms": 6,
+            "surface": 150,
+            "title": "Massive Flat Lille Vauban",
+            "type": "flat",
         },
     )
 
-    new_listing = ListingItem(**response.json())
+    listing_response = ListingItem(**response.json())
 
-    assert new_listing.id == 1
-    assert new_listing.listing_url == "seloger.com/massivehouselille"
-    assert new_listing.title == "Massive House Lille Vauban"
-    assert new_listing.latest_price == 400000
-    assert new_listing.location == "27 rue Solferino, Lille"
+    assert listing_response.id == 1
+    assert listing_response.listing_url == "seloger.com/massiveflatlille"
+    assert listing_response.title == "Massive Flat Lille Vauban"
+    assert listing_response.latest_price == 500000
+    assert listing_response.location == "28 rue Solferino, Lille"
 
     assert response.status_code == 201
