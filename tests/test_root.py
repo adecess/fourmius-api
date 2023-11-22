42a859e46494fb82c856071c051f626fd4d066e3
Axel Saliniere <axel.saliniere@protonmail.com>
2023-11-22 10:47:47 +0100

    tidy up root test

--- tests/test_root.py
+++ tests/test_root.py
@@ -2,6 +2,12 @@ from app import main
 
 
 def test_root(test_app):
+    # Given
+    # test_app
+
+    # When
     response = test_app.get("/")
+
+    # Then
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to Fourmius.io!", "environment": "dev", "testing": True}
