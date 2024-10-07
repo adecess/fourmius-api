00a75f241ee52d1f30699387a9b2dc975ad14d7c
Axel <47715040+adecess@users.noreply.github.com>
2024-10-07 09:31:08 +0200

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-/.venv/bin/alembic upgrade head && echo "Migrations complete" && /.venv/bin/fastapi devw src/main.py --port 8000 --host 0.0.0.0 --reload
+/.venv/bin/alembic upgrade head && echo "Migrations complete" && /.venv/bin/fastapi dev src/main.py --port 8000 --host 0.0.0.0 --reload
