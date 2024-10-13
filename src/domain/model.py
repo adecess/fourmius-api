4f55640352cfeb6f5c15e8926088e5c620ea903b
adecess <axel.saliniere@protonmail.com>
2024-10-13 20:39:03 +0200

    define domain aggregate properties

---
+++ src/domain/model.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+from datetime import date
+from typing import List
+from enum import Enum
+from uuid import UUID
+
+
+class PropertyType(Enum):
+    HOUSE = 1
+    APARTMENT = 2
+
+
+@dataclass(frozen=True)
+class PropertyCharacteristic:
+    address: str
+    postcode: str
+    city: str
+    type: PropertyType
+    surface: int
+    rooms: int
+
+
+@dataclass(frozen=True)
+class PropertyPrice:
+    price: int
+    price_date: date
+
+
+class Property:
+    def __init__(
+        self,
+        id: UUID,
+        characteristic: PropertyCharacteristic,
+        prices: List[PropertyPrice],
+        listing: str,
+        version_number: int = 0,
+    ):
+        self.id = id
+        self.version_number = version_number
+        self.characteristic = characteristic
+        self.prices = prices
+        self.latest_price = prices[0]
+        self.change_since_previous_price = None
+        self.listing = listing
